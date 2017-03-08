555260facff63eb5be30164773b99adca2de05ed
zcheng <zcheng@DESKTOP-9FGS6SE>
2017-03-08 20:04:04 +0100

    improve output redirecting of plugin

--- fr.emn.atlanmod.veriatl.launcher/src/fr/emn/atlanmod/veriatl/launcher/VeriATLLauncher.java
+++ fr.emn.atlanmod.veriatl.launcher/src/fr/emn/atlanmod/veriatl/launcher/VeriATLLauncher.java
@@ -3,11 +3,18 @@
  */
 package fr.emn.atlanmod.veriatl.launcher;
 
+import java.io.PrintStream;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 
 import fr.emn.atlanmod.atl2boogie.xtend.core.driver;
 import fr.emn.atlanmod.atl2boogie.xtend.ocl.TypeInference;
@@ -38,7 +45,10 @@ public class VeriATLLauncher implements ILaunchConfigurationDelegate {
 		Metamodels.register(context.inMetamodel());
 		Metamodels.register(context.outMetamodel());
 
-		
+		MessageConsole myConsole = findConsole("My Console");
+		MessageConsoleStream out = myConsole.newMessageStream();
+		VeriATLLauncher.setConsole(new PrintStream(out));
+		   
 		// Run transformation
 		if (context.mode() == Mode.EXEC) {
 			System.out.println("VeriATL: EXEC Successfully executed");
@@ -100,6 +110,22 @@ public class VeriATLLauncher implements ILaunchConfigurationDelegate {
 		System.out.println("Finished");
 	}
 	
+	private MessageConsole findConsole(String name) {
+	      ConsolePlugin plugin = ConsolePlugin.getDefault();
+	      IConsoleManager conMan = plugin.getConsoleManager();
+	      IConsole[] existing = conMan.getConsoles();
+	      for (int i = 0; i < existing.length; i++)
+	         if (name.equals(existing[i].getName()))
+	            return (MessageConsole) existing[i];
+	      //no console found, so create a new one
+	      MessageConsole myConsole = new MessageConsole(name, null);
+	      conMan.addConsoles(new IConsole[]{myConsole});
+	      return myConsole;
+	}
 	
 
+	private static void setConsole(PrintStream ps) {
+	    System.setOut(ps);
+	    System.setErr(ps);
+	}
 }
