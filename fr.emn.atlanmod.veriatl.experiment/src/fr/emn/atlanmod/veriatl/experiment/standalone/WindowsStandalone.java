ba680ac22ac584a930b4ff788f7f3645e81c0110
Zheng Cheng <cz0590@gmail.com>
2017-03-20 17:51:38 +0100

    refactor exp driver

--- fr.emn.atlanmod.veriatl.experiment/src/fr/emn/atlanmod/veriatl/experiment/standalone/WindowsStandalone.java
+++ fr.emn.atlanmod.veriatl.experiment/src/fr/emn/atlanmod/veriatl/experiment/standalone/WindowsStandalone.java
@@ -3,6 +3,10 @@
  */
 package fr.emn.atlanmod.veriatl.experiment.standalone;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.emf.common.util.URI;
 
 import fr.emn.atlanmod.atl2boogie.xtend.core.driver;
@@ -17,6 +21,25 @@ import localize.ocldecomposerDriver;
  */
 public class WindowsStandalone {
 
+	
+	static String[] mutants = new String[]{"AF2", "AR", "DB3", "DR1","MB6", "MF6"};
+	static String org = "ORG";
+	private static final Map<String, String> changes = createMap();
+
+    private static Map<String, String> createMap() {
+        Map<String, String> result = new HashMap<String, String>();
+        result.put("AF2", "RS2RS");
+        result.put("AR", "CS2RS");
+        result.put("DB3", "IS2IS");
+        result.put("DR1", "SM2SM");
+        result.put("MB6", "T2TB");
+        result.put("MF6", "T2TB");
+        
+        return Collections.unmodifiableMap(result);
+    }
+    
+	
+	
 	public static ContextConstruction init(String proj){
 		String moduleName = "HSM2FSM";
 		String s = "HSM";
@@ -54,16 +77,57 @@ public class WindowsStandalone {
 	}
 	
 	
-	
-	
-	
-	public static void main(String[] args) throws Exception {	
-		ContextConstruction context = init("HSM2FSM_GUI");
+	public static void genAll(String p) throws Exception{
+		
+		String proj = String.format("%s/%s", p, org);
+		ContextConstruction context = init(proj);
 		gen(context);
 		decompose(context);
-		String aRule = "T2TA";
-		IncrementalTasks.debugBoogie(context, aRule);
-		NormalTasks.debugBoogie(context);
+		
+		for(String trg : mutants){
+			proj = String.format("%s/%s", p, trg);
+			context = init(proj);
+			gen(context);
+			decompose(context);
+		}
+		
+	}
+	
+	public static void execNormal(String p) throws Exception{
+		String proj = String.format("%s/%s", p, org);
+		ContextConstruction context = init(proj);
+		NormalTasks.execBoogie(context);
+		
+		for(String trg : mutants){
+			proj = String.format("%s/%s", p, trg);
+			context = init(proj);
+			NormalTasks.execBoogie(context);
+		}
+	}
+	
+	public static void execIncremental(String p) throws Exception{
+		// prepare cache
+		String proj = String.format("%s/%s", p, org);
+		ContextConstruction context = init(proj);
+		NormalTasks.execBoogie(context);
+		
+		// copy cache
+		
+		for(String trg : mutants){
+			proj = String.format("%s/%s", p, trg);
+			context = init(proj);
+			String aRule = changes.get(trg);
+			IncrementalTasks.execBoogie(context, aRule);
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {	
+		String p = "HSM2FSM";
+		genAll(p);
+		
+		//execNormal(p);
+		execIncremental(p);
 	}
 
 }
