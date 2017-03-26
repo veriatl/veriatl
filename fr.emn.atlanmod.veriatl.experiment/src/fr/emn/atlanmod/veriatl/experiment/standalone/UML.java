4208ee03136e2d7ba5c68f1f89c8e0662dd92569
zcheng <zcheng@DESKTOP-9FGS6SE>
2017-03-26 13:46:52 +0200

    dummy

--- fr.emn.atlanmod.veriatl.experiment/src/fr/emn/atlanmod/veriatl/experiment/standalone/UML.java
+++ fr.emn.atlanmod.veriatl.experiment/src/fr/emn/atlanmod/veriatl/experiment/standalone/UML.java
@@ -3,6 +3,8 @@
  */
 package fr.emn.atlanmod.veriatl.experiment.standalone;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -14,30 +16,38 @@ import java.util.Map;
 public class UML {
 	static String[] folders = new String[]{"Auxu", "Caches", "Sub-goals"};
 	static String[] posts;
-	static String[] mutantsAdd = CreateMap.AddMutants();
+	//static String[] mutantsAdd = CreateMap.AddMutants();
+	//private static final Map<String, String> changesAdd = CreateMap.Add();
 	
-	//static String[] mutants = new String[]{"DR1"};
+	static String[] mutantsAdd = new String[]{"AR000"};
+	private static final Map<String, String> changesAdd = add();
 	static String org = "ORG";
 	static String proj = "UML";
-	private static final Map<String, String> changesAdd = CreateMap.Add();
+	
 
+	public static Map<String, String> add() {
+    	Map<String, String> result = new HashMap<String, String>();
+        result.put("AR000", "CopyAbstraction_mutated");
 
+        
+        return Collections.unmodifiableMap(result);
+    }
     
     
 	public static void main(String[] args) throws Exception {	
 		
 		WindowsStandalone.createTask(folders, mutantsAdd, org, changesAdd, "UML2UMLs", "UML", "UMLs");
 		// prepare cache
-		WindowsStandalone.incPostInit(proj);
+//		WindowsStandalone.incPostInit(proj);
 		
 		
 		
 
 		
-//		WindowsStandalone.incPostRegression(proj);
+		WindowsStandalone.incPostRegression(proj);
 //		WindowsStandalone.standardSubInit(proj);
 //		WindowsStandalone.incSubInit(proj);
 
-//		WindowsStandalone.BoogieIncPostInit(proj);
+		WindowsStandalone.BoogieIncPostInit(proj);
 	}
 }
