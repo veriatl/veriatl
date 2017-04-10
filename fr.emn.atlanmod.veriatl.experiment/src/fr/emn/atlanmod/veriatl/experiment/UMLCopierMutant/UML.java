0530b7a7c82613d961b318da690ff9c322465d2f
Zheng Cheng <cz0590@gmail.com>
2017-04-10 13:57:43 +0200

    update fix

--- fr.emn.atlanmod.veriatl.experiment/src/fr/emn/atlanmod/veriatl/experiment/UMLCopierMutant/UML.java
+++ fr.emn.atlanmod.veriatl.experiment/src/fr/emn/atlanmod/veriatl/experiment/UMLCopierMutant/UML.java
@@ -15,7 +15,7 @@ public class UML {
 	static String[] folders = new String[]{"Auxu", "Caches", "Sub-goals", "Boogie"};
 	static String[] posts;
 	static String org = "ORG";
-	static String proj = "UML";
+	static String proj = "UMLTest";
 	
     
 	public static void main(String[] args) throws Exception {		
@@ -26,10 +26,6 @@ public class UML {
 	
 	public static void testCodeGen() throws Exception{
 		WindowsStandalone.createTask(folders, null, org, null, "UML2UMLs", "UML", "UMLs");
-	
-		WindowsStandalone.cleanAll(proj);
-		
-		// post fine-grained incremental verification mode
 		WindowsStandalone.codeGen(proj);
 	}
 	
