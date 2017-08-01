b478fc26ede2b96006a7f7b9721798d31103aa4f
Zheng Cheng <cz0590@gmail.com>
2017-08-01 14:56:36 +0200

    refine boogie executor

--- fr.emn.atlanmod.veriatl.tools/src/fr/emn/atlanmod/veriatl/tools/DefaultCommand.java
+++ fr.emn.atlanmod.veriatl.tools/src/fr/emn/atlanmod/veriatl/tools/DefaultCommand.java
@@ -1,12 +1,8 @@
 package fr.emn.atlanmod.veriatl.tools;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,22 +37,7 @@ public class DefaultCommand implements Command {
         this.executable = executable;
     }
 
-    /**
-     * Prints a {@code inputStream} in a {@code printStream}.
-     *
-     * @param inputStream the stream to print
-     * @param printStream the stream where to print
-     *
-     * @throws IOException if an I/O error occurs
-     */
-    private static void printStream(InputStream inputStream, PrintStream printStream) throws IOException {
-        try (BufferedReader input = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = input.readLine()) != null) {
-                printStream.println("    > " + line);
-            }
-        }
-    }
+
 
     @Override
     public VerificationResult execute(ArrayList<String>  args) {
@@ -82,19 +63,18 @@ public class DefaultCommand implements Command {
             int exitValue = process.waitFor();
             long end = System.currentTimeMillis();
             long time = end - start;
-//          printStream(process.getInputStream(), System.out);
-//          printStream(new ByteArrayInputStream(Long.toString(time).getBytes(StandardCharsets.UTF_8)), System.out);
-            
-            
+          
             if (exitValue != 0) {
-            	BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
+            	BufferedReader input = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            	String cmd = "";
 				String line;
+				
 				while ((line = input.readLine()) != null) {
-					System.out.println(line);				
+					cmd += String.format("    > %s", line);			
 				}
 				
-                throw new RuntimeException("The execution ended with an error: " + exitValue + ". See the trace for more information");
+				r = new VerificationResult("ERROR", cmd, time);
+				
             }else{
 				BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
 				String boogieRes = "false";
@@ -125,13 +105,12 @@ public class DefaultCommand implements Command {
             return r;
         }
         catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+            return new VerificationResult("IOError", e.getStackTrace().toString(), -1);
         }
         catch (InterruptedException e) {
             e.printStackTrace();
             process.destroyForcibly();
-            return new VerificationResult("INTERUPTED", "error", -1);
+            return new VerificationResult("INTERUPTED", e.getStackTrace().toString(), -1);
         }
     }
 }
