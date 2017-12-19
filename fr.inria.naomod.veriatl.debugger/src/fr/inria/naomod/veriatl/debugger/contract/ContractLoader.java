f7734241170634dbd6a7714e76701b954d147788
Zheng Cheng <cz0590@gmail.com>
2017-12-19 15:46:41 +0100

    update

--- fr.inria.naomod.veriatl.debugger/src/fr/inria/naomod/veriatl/debugger/contract/ContractLoader.java
+++ fr.inria.naomod.veriatl.debugger/src/fr/inria/naomod/veriatl/debugger/contract/ContractLoader.java
@@ -1,45 +1,33 @@
 package fr.inria.naomod.veriatl.debugger.contract;
 
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.m2m.atl.common.ATL.Helper;
 import org.eclipse.m2m.atl.common.ATL.Module;
-import org.eclipse.m2m.atl.common.ATL.ModuleElement;
 import org.eclipse.m2m.atl.common.OCL.OclExpression;
 import org.eclipse.m2m.atl.common.OCL.Operation;
 
 public class ContractLoader {
 
-
 	public static List<OclExpression> init(Resource m) throws Exception{
 
 		
-		ArrayList<OclExpression> rtn = new ArrayList<OclExpression>();
-		
-		for(EObject o : m.getContents()){
-			if(o instanceof Module){
-				Module mod = (Module) o;
-				
-				for(ModuleElement elem : mod.getElements()){
-					if(elem instanceof Helper){
-						Helper h = (Helper) elem;		
-						
-						if(h.getDefinition().getFeature() instanceof Operation){
-							if(((Operation)h.getDefinition().getFeature()).getName().startsWith("post_")){
-								
-								Operation contract = (Operation) h.getDefinition().getFeature();
-								rtn.add(contract.getBody());	
-							}							
-						}
-						
-					}
-				}
-			}
-		}
+		List<OclExpression> rtn  = m.getContents().stream()
+				               .filter(Module.class::isInstance)
+				               .map(Module.class::cast)
+				               .map(Module::getElements)
+				               .flatMap(List::stream)
+				               .filter(Helper.class::isInstance)
+				               .map(Helper.class::cast)
+				               .map(hlp -> hlp.getDefinition().getFeature())
+				               .filter(Operation.class::isInstance)
+				               .map(Operation.class::cast)
+				               .filter(op -> op.getBody().getCommentsBefore().stream().anyMatch(c -> c.contains("@post ")))
+				               .map(Operation::getBody)
+				               .collect(Collectors.toList());
 		
 		return rtn;
 	}
